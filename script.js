89bf8354e6421070d7a699713aaeb6163c35cc98
John Heger <heger7@gmail.com>
2023-09-10 11:10:43 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by Javascript";
+document.querySelector("body").appendChild(h2);
+
+const h1 = document.createElement("h1");
+h1.textContent = "Adding another title block";
+document.querySelector("body").appendChild(h1);
