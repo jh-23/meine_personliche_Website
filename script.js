55c68081cdd7db11dcb125c901dd8674d2fdbec2
John Heger <heger7@gmail.com>
2023-09-11 14:07:17 -0500

    Updated JavaScript file with mouseover event and rearranged some

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Aspiring Software Engineer";
-document.querySelector("body").appendChild(h2);
 
-
-
-
-
-
+const img = document.getElementById("first-image");
+console.log(img);
+img.addEventListener('mouseover', function(){
+    img.src="https://lh3.googleusercontent.com/pw/AIL4fc9i_IbK1Fvb-m_yQpncgiOlan8pijurUDiHiVq-C53S4Wn8ZtNDD7jKzUlqGJHuUGDwXAH4KwyY2oD08i-c3NFwoB19JXjPjy3OffyYM_aNR8rXnNI=w2400"
+})
+img.addEventListener('mouseout', function(){
+    img.src="https://lh3.googleusercontent.com/pw/AIL4fc-FMDGAkY7Z5QvyDDktCs2tNBIkfgeyFTLfjq5Mr7jUiFi7MmgQm8fL0v09EIAFqSzqhaSrzkOiehop2PFfJ9mH8bJZq0RMZmNRwQNxX6-JpuAkXdo=w2400"
+})
 
