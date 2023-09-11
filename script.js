a4bf72420cba523904a38019d380f3a8c6ad6c02
John Heger <heger7@gmail.com>
2023-09-11 10:38:43 -0500

    Added image, background, rearranged text

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "Aspiring Software Engineer";
 document.querySelector("body").appendChild(h2);
 
-const h1 = document.createElement("h1");
-h1.textContent = "Adding another title block";
-document.querySelector("body").appendChild(h1);
+
+
+
+
+
+
